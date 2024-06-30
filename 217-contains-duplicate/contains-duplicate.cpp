a9776fbcc45081ac78b6ac75bd8061f7d33dcfdb
Jessie Le <138086928+jessiele2003@users.noreply.github.com>
2024-06-29 22:31:12 -0700

    Time: 100 ms (38.42%) | Memory: 73.1 MB (63.17%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> uniqueNums;
+        for(int i = 0; i < nums.size(); ++i) {
+            if (uniqueNums.find(nums[i]) != uniqueNums.end())
+                return true;
+            uniqueNums.insert(nums[i]);
+        }
+        return false;
+    }
+};
