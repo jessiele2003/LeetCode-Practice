af2ff6370ed93988d280eb6106742772dcb14a9c
Jessie Le <138086928+jessiele2003@users.noreply.github.com>
2024-07-06 13:05:23 -0700

    Time: 4 ms (90.35%) | Memory: 10.2 MB (5.86%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       //store 2 strings into 2 hashmaps
-       unordered_map<char, int> uniqueS;
-       unordered_map<char, int> uniqueT;
-
-       //check length of 2 strings
-       if(s.size() != t.size())
+        // 1. Check the 2 strings length -> if not, then definitely not anagrams
+        if(s.size() != t.size())
             return false;
         
-        //store the letters into the hash tables
+        unordered_map<char, int> uniqueS;
+        unordered_map<char, int> uniqueT;
+        // 2. Loop through and insert two strings into two hash tables
         for(int i = 0; i < s.size(); ++i) {
             uniqueS[s[i]]++;
             uniqueT[t[i]]++;
         }
 
+        // 3. Loop to count the unique characters
         for(int i = 0; i < uniqueS.size(); ++i) {
             if(uniqueS[i] != uniqueT[i])
                 return false;
