1a739d4d50c535f11b85550d58caf028bc3745dc
Jessie Le <138086928+jessiele2003@users.noreply.github.com>
2024-08-27 13:59:40 -0700

    Time: 12 ms (13.58%) | Memory: 17.6 MB (83.06%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -4,9 +4,10 @@ public:
         int count = 0;
         for(const auto& d : details)
         {
+            //convert string to interger using Ascii values
             int tenth = d[11] - '0';
             int one = d[12] - '0';
-            int age = one + tenth*10;
+            int age = one + tenth*10;   //tenth * 10 because example we want 40 so 4 * 10
             if (age > 60)
                 count += 1;
         }
