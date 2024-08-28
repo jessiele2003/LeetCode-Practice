b5bff5a5489462137d63337d5f4d77cb72eb3e04
Jessie Le <138086928+jessiele2003@users.noreply.github.com>
2024-08-28 15:53:30 -0700

    Time: 2 ms (49.24%) | Memory: 11 MB (85.38%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        //find the last index of nums1 array
         int last = m + n -1;
         
-        while( m > 0 && n > 0) {
-            if(nums1[m - 1] < nums2[n - 1]) {
+        while (m > 0 && n > 0) {
+            if(nums1[m-1] < nums2[n-1]) {
                 nums1[last] = nums2[n-1];
                 n -=1;
             }
             else {
                 nums1[last] = nums1[m-1];
-                m -=1;
+                m -= 1;
             }
             last -= 1;
         }
 
-        //fill up the smaller number is in nums2
-        while (n > 0) {
+        //fill up left over nums2 into nums1
+        while( n > 0) {
             nums1[last] = nums2[n-1];
-            n -=1;
-            last -=1;
+            n -= 1;
+            last -= 1;
         }
     }
 };
-
-/* -1 because m and n represents the valid elements of arrays, not the indexes.
-The array index starts at 0, so that the last index should be -1
-*/
