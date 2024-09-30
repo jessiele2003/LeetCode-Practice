7ea68e8a07f5babe9007c34e00b251e9821994e4
Jessie Le <138086928+jessiele2003@users.noreply.github.com>
2024-09-29 18:00:53 -0700

    Time: 0 ms (100.00%) | Memory: 11.3 MB (68.94%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int last = m + n -1;
-        
-        while (m > 0 && n > 0) {
+        int last = m + n -1;   //the last index of nums1
+
+        while(m > 0 && n >0) {
             if(nums1[m-1] < nums2[n-1]) {
                 nums1[last] = nums2[n-1];
-                n -=1;
+                n-=1;
             }
             else {
                 nums1[last] = nums1[m-1];
                 m -=1;
             }
             last -=1;
-        }
 
-        //if in the end nums2 is larger than nums1
-        while(n >0) {
+        }
+        while (n >0) {
             nums1[last] = nums2[n-1];
             n -=1;
             last -=1;
