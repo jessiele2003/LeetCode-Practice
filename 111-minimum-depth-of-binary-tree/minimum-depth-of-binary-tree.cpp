657cc04d36f750491560de61b99f8aeaafc35cec
Jessie Le <138086928+jessiele2003@users.noreply.github.com>
2024-10-21 16:41:31 -0700

    Time: 3 ms (98.26%) | Memory: 145.1 MB (41.18%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -14,11 +14,18 @@ public:
     int minDepth(TreeNode* root) {
         if(!root) return 0;
 
+        //if the subtree exists
+        // current node has one child
+        
+        //if current node is null
         if(!root->left)
-            return 1 + minDepth(root->right);
+            return 1 + minDepth(root->right);   //plus one for the current node and the minimum of right subtree
         if(!root->right)
-            return 1 + minDepth(root->left);
+            return 1 + minDepth(root->left); //plus one for the current node and the minimum of left subtree
 
+        //recursive call
+        //if current node has both children
+        //ensures that the function finds the shortest path to a leaf node
         return 1 + min(minDepth(root->right), minDepth(root->left));
     }
 };
