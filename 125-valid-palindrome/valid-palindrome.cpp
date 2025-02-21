4b581c5ef158cd06515c4b8218ef04fe1727fdbe
Jessie Le <138086928+jessiele2003@users.noreply.github.com>
2025-02-20 16:28:23 -0800

    Time: 1 ms (48.57%) | Memory: 10.8 MB (6.23%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int l = 0;   //left most the string
-        int r = s.size() -1;    //right most the string
-
-
-        //check to make sure the left never pass the right
-        while (l < r) {
-            while (l < r && !isalnum(s[l]))    //if the left is not alphabetical, then increase 1
-            {
-                ++l;
-            }
-            while (l < r && !isalnum(s[r])) //if the right is not alphabetical, then decrease the index => go backward
-            {
-                --r;
-            }
-            if(tolower(s[l]) != tolower(s[r])) {    //if the current pair not match
-                return false;
-            }
-            ++l;    //keep going until the end of the loop
-            --r;
+        string t;
+        for (auto& ch : s){
+            if(isalnum(ch)) t+= tolower(ch);
         }
-        return true;
+        swap(s,t);
+        string a = s;
+        reverse(s.begin(), s.end());
+        return a == s;
+        cout << s << endl;
+        return false;
     }
 };
