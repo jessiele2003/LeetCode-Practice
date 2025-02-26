175aa23e20d05980f30c1c5670710ff8dd2308d6
Jessie Le <138086928+jessiele2003@users.noreply.github.com>
2025-02-25 16:08:50 -0800

    Time: 3 ms (92.34%) | Memory: 10.1 MB (48.66%) - LeetSync

---
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool hasSameDigits(string s) {
+        //3902
+        //ij       (3 + 9) % 10 = 2
+        // ij       (9 + 0) % 10 = 9
+        //  ij      (0 + 2) % 10 = 2
+        if(s.length() < 2)
+            return false;
+        while(s.length() >= 2) {
+            string num;
+            //cout << "Length: " << s.length() << endl;
+            if(s.length() == 2) {
+                return s[0] == s[1];
+            } 
+            else {
+                for(int i = 0; i < s.size() -1 ; ++i) {
+                    int j = i +1;
+        
+                    char result = ((s[i] - '0' + s[j] - '0') % 10) + '0';
+                    
+                    //cout << "S[i]: " << s[i] << "Index i" << i <<endl;
+                    //cout << "S[j]: " << s[j] << "Index j" << j<<endl;
+                    //cout << "Result: " << result << endl;
+                    
+                    num.push_back(result);
+                    //cout << num << endl;
+                }
+            }
+            s = num;
+        }
+        return false;
+    }
+};
